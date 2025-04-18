589f51e435ba042bf0e6efe704e02d3daed5ad72
Anthony Vargas <Av491609@gmail.com>
2025-04-18 06:43:43 +0000

    Added user class

---
+++ User.java
@@ -0,0 +1,14 @@
+public class User {
+
+    private String name;
+
+    public User() {}
+
+    public User(String name) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
