63a6411299cfdd46a6bb7f7f50e27dd37ddcf0a5
Anthony Vargas <Av491609@gmail.com>
2025-04-20 04:44:37 +0000

    added user variables

--- User.java
+++ User.java
@@ -1,11 +1,17 @@
 public class User {
 
     private String name;
+    private long weight;
+    private long height;
+    private String activity;
+    private String goal;
+    private long bodyFat_percentage;
+    private int protein_goal;
 
     public User() {}
 
     public User(String name) {
-        
+        this.name = name;
     }
 
     public static void main(String[] args) {
