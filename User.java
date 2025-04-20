e31f2215edb649106bafa5c44420419cb69c1693
Anthony Vargas <Av491609@gmail.com>
2025-04-20 05:08:40 +0000

    introduced first calculate function

--- User.java
+++ User.java
@@ -1,11 +1,12 @@
 public class User {
 
     private String name;
-    private long weight;
-    private long height;
+    private double weight;
+    private double height;
     private String activity;
     private String goal;
-    private long bodyFat_percentage;
+    private double bodyFat_percentage;
+    private double protein_multiplier;
     private int protein_goal;
 
     public User() {}
@@ -14,7 +15,23 @@ public class User {
         this.name = name;
     }
 
+    public User(String name, double weight, double height) {
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int calculate_Intake() {
+        int weightKg = (int) (weight / 2.2);
+        protein_multiplier = 2.0;
+
+        return (int) (weightKg * protein_multiplier);
+    }
+    
+
     public static void main(String[] args) {
         
+        User tony = new User("tony", 200.5, 182.88);
+        System.out.println(tony.calculate_Intake());
     }
 }
